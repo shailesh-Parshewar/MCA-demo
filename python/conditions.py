94cd76089f5b463b41956d8737b2f08b37fa93fb
shailesh-Parshewar <srparshewar@gmail.com>
2026-09-15 10:26:38 +0530

    Bridge Course Day 3 - 4

--- python/conditions.py
+++ python/conditions.py
@@ -180,3 +180,40 @@
 #         print("remainder of a divided by b : ", a % b)
 #     case _:
 #         print("invalid op")
+
+# # Example 10 : Traffic signal
+#  a = input("Enter a signal(Red, Yellow, Green) : ")
+#  match a:
+#     case "Red":
+#         print("Stop")
+#     case "Yellow":
+#         print("Get ready")
+#     case "Green":
+#         print("Go")
+#     case _:
+#         print("Invalid color")
+
+# Example 11: ATM Menu
+balance = int(input("Enter your balance : "))
+
+while True:
+    action = input("Enter an action(Withdraw, Deposit, Check Balance, Exit) : ")
+    match action:
+        case "Withdraw":
+            amt = int(input("Enter amount to withdraw : "))
+            if amt > balance:
+                print("Amt is invalid: (Code : Amt is bigger than remaining balance)")
+            else:
+                balance = balance - amt
+                print("Remaining balance : ", balance)
+        case "Deposit":
+            amt = int(input("Enter amt to deposit : "))
+            balance =+ amt
+            print("New Balance is : ", balance)
+        case "Check Balance":
+            print("Your Balance is : ", balance)
+        case "Exit":
+            break
+        case _:
+            print("Invalid action : (Code : Action not recognized)")
+
